51bb407a63c99c21084aee9739b0c6a1663f964e
nerdy-plutonian-ng <nerdymykl@gmail.com>
2023-01-08 18:42:07 +0000

    added attribution file added splash screen

---
+++ lib/data/attributions.dart
@@ -0,0 +1,6 @@
+import 'package:dark_forest/data/models/attribution.dart';
+
+const attributions = [
+  Attribution('prettycons',
+      '<a href="https://www.flaticon.com/free-icons/forest" title="forest icons">Forest icons created by prettycons - Flaticon</a>'),
+];
---
+++ lib/data/models/attribution.dart
@@ -0,0 +1,6 @@
+class Attribution {
+  final String creator;
+  final String link;
+
+  const Attribution(this.creator, this.link);
+}
